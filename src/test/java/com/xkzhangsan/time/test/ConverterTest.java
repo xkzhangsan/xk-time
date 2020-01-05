c14f9fd5866c55dfdcf934797c2661a2f4ae1f1a
xkzhangsan <xkzhangsan@163.com>
2020-01-05 22:43:56 +0800

    增加测试类

--- src/test/java/com/xkzhangsan/time/test/ConverterTest.java
+++ src/test/java/com/xkzhangsan/time/test/ConverterTest.java
@@ -3,6 +3,7 @@ package com.xkzhangsan.time.test;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 
 import org.junit.Test;
@@ -13,6 +14,7 @@ public class ConverterTest {
 	
 	@Test
 	public void dateConverterTest(){
+		System.out.println("===================dateConverterTest=====================");
 		Date date = new Date();
 		System.out.println(DateTimeConverterUtil.toLocalDateTime(date));
 		System.out.println(DateTimeConverterUtil.toLocalDate(date));
@@ -22,6 +24,7 @@ public class ConverterTest {
 	
 	@Test
 	public void localDateTimeConverterTest(){
+		System.out.println("===================localDateTimeConverterTest=====================");
 		LocalDateTime ldt = LocalDateTime.now();
 		System.out.println(ldt);
 		System.out.println(DateTimeConverterUtil.toDate(ldt));
@@ -32,16 +35,29 @@ public class ConverterTest {
 	
 	@Test
 	public void localDateConverterTest(){
+		System.out.println("===================localDateConverterTest=====================");
 		LocalDate ld = LocalDate.now();
 		System.out.println(ld);
 		System.out.println(DateTimeConverterUtil.toDate(ld));
 		System.out.println(DateTimeConverterUtil.toLocalDateTime(ld));
 		System.out.println(DateTimeConverterUtil.toInstant(ld));
 	}
+	
+	@Test
+	public void localTimeConverterTest(){
+		System.out.println("===================localTimeConverterTest=====================");
+		LocalTime lt = LocalTime.now();
+		System.out.println(lt);
+		System.out.println(DateTimeConverterUtil.toDate(lt));
+		System.out.println(DateTimeConverterUtil.toLocalDateTime(lt));
+		System.out.println(DateTimeConverterUtil.toLocalTime(lt));
+		System.out.println(DateTimeConverterUtil.toInstant(lt));
+	}	
 
 	
 	@Test
 	public void instantConverterTest(){
+		System.out.println("===================instantConverterTest=====================");
 		Instant instant = Instant.now();
 		System.out.println(instant);
 		System.out.println(DateTimeConverterUtil.toDate(instant));
