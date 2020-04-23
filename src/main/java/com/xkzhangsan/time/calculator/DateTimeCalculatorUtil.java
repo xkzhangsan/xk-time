817f3057ae62f5aa77f6fdec078cd44f1f9bcca8
xkzhangsan <xkzhangsan@163.com>
2020-04-23 15:28:21 +0800

    readme修改

--- src/main/java/com/xkzhangsan/time/calculator/DateTimeCalculatorUtil.java
+++ src/main/java/com/xkzhangsan/time/calculator/DateTimeCalculatorUtil.java
@@ -49,6 +49,7 @@ import com.xkzhangsan.time.formatter.DateTimeFormatterUtil;
  * 10.相同月日比较判断方法，isSameMonthDay*，betweenNextSameMonthDay*，nextSameMonthDay*， 比如用于生日，节日等周期性的日期比较判断<br>
  * 11.星座计算方法，getConstellation*，比如getConstellationNameCn(String monthDayStr)，根据日期计算星座<br>
  * 12.计算指定年月或起始时间区间的时间列表，get*List， 比如getDateList(int year, int month)，计算指定年月的时间列表<br>
+ * 13.减少时间精度方法，reduceAccuracyTo*，， 比如reduceAccuracyToDay(Date date)，减少时间精度到天，其他补0，返回如，2020-04-23 00:00:00<br>
 * @ClassName: DateTimeCalculatorUtil 
 * @Description:  DateTime Calculator
 * @author xkzhangsan
