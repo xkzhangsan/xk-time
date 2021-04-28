58492d17485bbc5082947d825077c2c77a5c092c
xkzhangsan <xkzhangsan@163.com>
2021-04-28 23:57:28 +0800

    单位转换计算

--- src/main/java/com/xkzhangsan/time/constants/Constant.java
+++ src/main/java/com/xkzhangsan/time/constants/Constant.java
@@ -15,34 +15,64 @@ public class Constant {
 	
 	//===========================数值定义============================
 	/**
-	 * 每天小时数
+	 * 每周天数 7
+	 */
+	public static final int DAY_PER_WEEK = 7;
+	
+	/**
+	 * 每天小时数 24
 	 */
 	public static final int HOURS_PER_DAY = 24;
 	
 	/**
-	 * 每小时分钟数
+	 * 每小时分钟数 60
 	 */
 	public static final int MINUTES_PER_HOUR = 60;
 	
 	/**
-	 * 每分钟秒数
+	 * 每分钟秒数 60
 	 */
 	public static final int SECONDS_PER_MINUTE = 60;
 	
 	/**
-	 * 每秒毫秒数
+	 * 每秒毫秒数 1000
 	 */
 	public static final int MILLISECOND_PER_SECONDS = 1000;
 	
 	/**
-	 * 每天秒数
+	 * 每小时秒数 60*60
 	 */
-	public static final int SECONDS_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_MINUTE;
+	public static final int SECONDS_PER_HOUR = MINUTES_PER_HOUR * SECONDS_PER_MINUTE;
 	
 	/**
-	 * 每天毫秒数
+	 * 每天秒数 24*60*60
 	 */
-	public static final int MILLISECOND_PER_DAY = SECONDS_PER_DAY * MILLISECOND_PER_SECONDS;
+	public static final int SECONDS_PER_DAY = HOURS_PER_DAY * SECONDS_PER_HOUR;
+	
+	/**
+	 * 每周秒数 7*24*60*60
+	 */
+	public static final int SECONDS_PER_WEEK = DAY_PER_WEEK * SECONDS_PER_DAY;
+	
+	/**
+	 * 每分钟毫秒数 60*1000
+	 */
+	public static final int MILLISECOND_PER_MINUTE = SECONDS_PER_MINUTE * MILLISECOND_PER_SECONDS;
+	
+	/**
+	 * 每小时毫秒数 60*60*1000
+	 */
+	public static final int MILLISECOND_PER_HOUR = MINUTES_PER_HOUR * MILLISECOND_PER_MINUTE;
+	
+	/**
+	 * 每天毫秒数 24*60*60*1000
+	 */
+	public static final int MILLISECOND_PER_DAY = HOURS_PER_DAY * MILLISECOND_PER_HOUR;
+	
+	/**
+	 * 每星期毫秒数 7*24*60*60*1000
+	 */
+	public static final int MILLISECOND_PER_WEEK = DAY_PER_WEEK * MILLISECOND_PER_DAY;
 	
 	//===========================异常定义============================
 	/**
