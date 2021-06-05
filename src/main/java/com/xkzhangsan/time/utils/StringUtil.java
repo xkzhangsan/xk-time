d84b58d8ef8abcb3c3ed1d3fb9a906f61ed58792
xkzhangsan <xkzhangsan@163.com>
2021-06-05 21:58:31 +0800

    工作日计算，使用缓存优化

--- src/main/java/com/xkzhangsan/time/utils/StringUtil.java
+++ src/main/java/com/xkzhangsan/time/utils/StringUtil.java
@@ -2,6 +2,7 @@ package com.xkzhangsan.time.utils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 import com.xkzhangsan.time.constants.Constant;
 
@@ -98,5 +99,42 @@ public class StringUtil {
 			dateTypeMap.put(arr[0], Integer.valueOf(arr[1]));
 		}
 		return dateTypeMap;
+	}
+
+    /**
+     * 转换节日数据为map，使用缓存提高性能
+     * @param holidayData 节日map
+     * @return 返回节日map
+     */
+	@SuppressWarnings("unchecked")
+	public static Map<String, Integer> convertHolidayDataToMapUseCache(String holidayData){
+		Map<String, Integer> dateTypeMap = new HashMap<>();
+		//参数为空，直接返回
+		if(isEmpty(holidayData)){
+			return dateTypeMap;
+		}
+		
+		//查询缓存
+		dateTypeMap = (Map<String, Integer>)CommonCache.get(holidayData);
+		
+		//缓存存在，返回缓存
+		if(CollectionUtil.isNotEmpty(dateTypeMap)){
+			return dateTypeMap;
+		}
+		
+		//缓存不存在，先设置缓存然后返回
+		Supplier<Object> supplier = new Supplier<Object>() {
+			@Override
+			public Object get() {
+				Map<String, Integer> dateTypeMap = new HashMap<>();
+				String[] dateTypeArr = holidayData.replace(" ", "").split(",");
+				for(String dateType : dateTypeArr){
+					String[] arr = dateType.split(":");
+					dateTypeMap.put(arr[0], Integer.valueOf(arr[1]));
+				}
+				return dateTypeMap;
+			}
+		};
+		return (Map<String, Integer>)CommonCache.get(holidayData, supplier);
 	}
 }
